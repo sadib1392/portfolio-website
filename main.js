3aa41945d6dae4dfda7ffb5b6b46ad20da918c08
Syed Adib <sadib1392@bths.edu>
2022-07-28 11:15:55 -0400

    Added some responsiveness, svgs, and increased element sizes

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ var contactFormTl = gsap.timeline();
 
 const headerAnimations = document.querySelectorAll('.header-animation');
 const projectCardAnimations = document.querySelectorAll('.project-card-animation');
+const projectCardAirplanePaths = document.querySelectorAll('.st3');
 const experienceCardAnimations = document.querySelectorAll('.experience-card-animation');
 const skillsTextAnimations = document.querySelectorAll('.skills-text-animation');
 const skillsCardAnimations = document.querySelectorAll('.skills-card-animation');
@@ -22,6 +23,10 @@ headerAnimations.forEach(header => {
 
 projectCardAnimations.forEach(card => {
     card.style.opacity = 0;
+})
+
+projectCardAirplanePaths.forEach(path => {
+    path.style.opacity = 0;
 })
 
 experienceCardAnimations.forEach(card => {
@@ -143,11 +148,14 @@ var headerTl = gsap.timeline();
 
 gsap.fromTo("#logo", {opacity: 0, y: -50}, {opacity: 1, duration: 1, y: 0});
 gsap.fromTo(".nav-link", {opacity: 0, y: -50}, {opacity: 1, duration: 1, y: 0});
+gsap.fromTo('.st3', {opacity: 0}, {opacity: 1, stagger: .2, duration: 2});
+
+
 
 headerTl.fromTo(".hero-image-text", {opacity: 0, y: 100}, {opacity: 1, duration: 1, y: 0});
 headerTl.fromTo(".cta", {opacity: 0, y: 100}, {opacity: 1, duration: 1, y: 0});
 
 
 
-
+console.log(projectCardAirplanePaths);
 
